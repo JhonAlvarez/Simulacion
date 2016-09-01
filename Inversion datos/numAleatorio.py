b15afc20cb50a0cd143ed6a506a15c166ea2bd9c
jalvarezo <jhonalvarez_sas@hotmail.com>
2016-08-31 19:42:40 -0500

    Laboratorio 4
    Inversion de datos

---
+++ Inversion datos/numAleatorio.py
@@ -0,0 +1,27 @@
+
+# coding: utf-8
+
+# In[2]:
+
+import numpy as np
+
+def Conmix(numax=1):
+    numeros= []
+    Xn = 7
+    a = 7
+    c = 1
+    m = 16547
+    
+    
+    for iterator in range(numax):        
+        Xn_1 = (a*Xn + c)% m        
+        out = float(Xn_1)/float(m)    
+        numeros.append(out)
+        Xn = Xn_1
+    return numeros
+
+
+# In[ ]:
+
+
+
